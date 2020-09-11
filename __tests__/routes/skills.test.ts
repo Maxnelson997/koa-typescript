30a2d680e1b9723ec0e830b99b15bcf767f1b635
Max <maxnelson997@gmail.com>
2020-09-10 21:04:08 -0600

    added delete route and appropriate tests

--- __tests__/routes/skills.test.ts
+++ __tests__/routes/skills.test.ts
@@ -10,108 +10,194 @@ afterEach(done => {
 });
 
 describe("routes/skills", () => {
-  const skills = [
-    "Agile Development",
-    "React"
-    // "Webpack",
-    // "Babel",
-    // "ESLint",
-    // "Angular",
-    // "Vue",
-    // "Redux",
-    // "Hooks"
-  ];
+  describe("post", () => {
+    const skills = [
+      "Agile Development",
+      "React"
+      // "Webpack",
+      // "Babel",
+      // "ESLint",
+      // "Angular",
+      // "Vue",
+      // "Redux",
+      // "Hooks"
+    ];
 
-  skills.forEach((skill: string) => {
-    it(`should allow adding a skill to the list - ${skill}`, async () => {
-      // storage.redisStorage = jest.fn(() => {
+    skills.forEach((skill: string) => {
+      it(`should allow adding a skill to the list - ${skill}`, async () => {
+        // storage.redisStorage = jest.fn(() => {
 
-      // });
-      const mockGet = jest.fn((list: string) => Promise.resolve([skill]));
+        // });
+        const mockGet = jest.fn((list: string) => Promise.resolve([skill]));
+
+        storage.redisStorage = jest.fn(() => {
+          return {
+            get: mockGet,
+            add: (list: string) => Promise.resolve(false),
+            remove: (list: string) => Promise.resolve(false)
+          };
+        });
+
+        const response = await request(server)
+          .post("/skills")
+          .send({ skill });
+
+        expect(response.status).toEqual(201);
+        expect(response.type).toEqual("application/json");
+        expect(response.body).toEqual({
+          skills: [skill]
+        });
+
+        expect(mockGet).toHaveBeenCalled();
+      });
+    });
+
+    it(`should keep track of all skills added to the list'`, async () => {
+      const list_of_skills: string[] = [];
+      const mockGet = jest.fn((list: string) =>
+        Promise.resolve(list_of_skills)
+      );
+      const mockAdd = jest.fn((list: string, name: string) => {
+        list_of_skills.push(name);
+        return list_of_skills.length > 0;
+      });
 
       storage.redisStorage = jest.fn(() => {
         return {
           get: mockGet,
-          add: (list: string) => Promise.resolve(false),
+          add: mockAdd,
           remove: (list: string) => Promise.resolve(false)
         };
       });
 
-      const response = await request(server)
+      const data1 = { skill: "Docker" };
+      const response1 = await request(server)
         .post("/skills")
-        .send({ skill });
+        .send(data1);
 
-      expect(response.status).toEqual(201);
-      expect(response.type).toEqual("application/json");
-      expect(response.body).toEqual({
-        skills: [skill]
+      expect(response1.status).toEqual(201);
+      expect(response1.type).toEqual("application/json");
+      expect(response1.body).toEqual({
+        skills: [data1.skill]
       });
 
-      expect(mockGet).toHaveBeenCalled();
+      const data2 = { skill: "Vue" };
+      const response2 = await request(server)
+        .post("/skills")
+        .send(data2);
+
+      expect(response2.status).toEqual(201);
+      expect(response2.type).toEqual("application/json");
+      expect(response2.body).toEqual({
+        skills: [data1.skill, data2.skill]
+      });
+    });
+
+    xit("should return a validation failure if the skill data is incorrect", async () => {
+      const response = await request(server)
+        .post("/skills")
+        .send({ skill: "" });
+
+      expect(response.status).toEqual(400);
+      expect(response.type).toEqual("application/json");
+      expect(response.body).toEqual({
+        status: "error",
+        data: [
+          {
+            target: {
+              skill: ""
+            },
+            value: "",
+            property: "skill",
+            children: [],
+            constraints: {
+              length: "skill must be longer than or equal to 1 characters"
+            }
+          }
+        ]
+      });
     });
   });
 
-  it(`should keep track of all skills added to the list'`, async () => {
-    const list_of_skills: string[] = [];
+  describe("delete", () => {
+    it("returns an empty list when the list is empty", async () => {
+      const skill = "C++";
+
+      const list_of_skills: string[] = [skill];
+
+      const mockGet = jest.fn((list: string) =>
+        Promise.resolve(list_of_skills)
+      );
+      const mockAdd = jest.fn();
+      const mockRemove = jest.fn((list: string, skill: string) => {
+        const index = list_of_skills.indexOf(skill);
+        if (index === -1) {
+          return false;
+        }
+        list_of_skills.splice(index, 1);
+        return true;
+      });
+
+      storage.redisStorage = jest.fn(() => {
+        return {
+          get: mockGet,
+          add: mockAdd,
+          remove: mockRemove
+        };
+      });
+
+      const response = await request(server)
+        .delete("/skills")
+        .send({ name: skill });
+
+      expect(response.status).toEqual(200);
+      expect(response.type).toEqual("application/json");
+      expect(response.body).toEqual({
+        skills: []
+      });
+
+      expect(mockGet).toHaveBeenCalled();
+      expect(mockRemove).toHaveBeenCalled();
+      expect(mockAdd).not.toHaveBeenCalled();
+    });
+  });
+
+  it("returns an updated list when deleting a skill", async () => {
+    const skill = "Overwatch";
+
+    const list_of_skills: string[] = ["GTA 5", skill, "Diablo 3"];
+
     const mockGet = jest.fn((list: string) => Promise.resolve(list_of_skills));
-    const mockAdd = jest.fn((list: string, name: string) => {
-      list_of_skills.push(name);
-      return list_of_skills.length > 0;
+    const mockAdd = jest.fn();
+    const mockRemove = jest.fn((list: string, skill: string) => {
+      const index = list_of_skills.indexOf(skill);
+      if (index === -1) {
+        return false;
+      }
+      list_of_skills.splice(index, 1);
+      return true;
     });
 
     storage.redisStorage = jest.fn(() => {
       return {
         get: mockGet,
         add: mockAdd,
-        remove: (list: string) => Promise.resolve(false)
+        remove: mockRemove
       };
     });
 
-    const data1 = { skill: "Docker" };
-    const response1 = await request(server)
-      .post("/skills")
-      .send(data1);
-
-    expect(response1.status).toEqual(201);
-    expect(response1.type).toEqual("application/json");
-    expect(response1.body).toEqual({
-      skills: [data1.skill]
-    });
-
-    const data2 = { skill: "Vue" };
-    const response2 = await request(server)
-      .post("/skills")
-      .send(data2);
-
-    expect(response2.status).toEqual(201);
-    expect(response2.type).toEqual("application/json");
-    expect(response2.body).toEqual({
-      skills: [data1.skill, data2.skill]
-    });
-  });
-
-  xit("should return a validation failure if the skill data is incorrect", async () => {
     const response = await request(server)
-      .post("/skills")
-      .send({ skill: "" });
+      .delete("/skills")
+      .send({ name: skill });
 
-    expect(response.status).toEqual(400);
+    expect(response.status).toEqual(200);
     expect(response.type).toEqual("application/json");
     expect(response.body).toEqual({
-      status: "error",
-      data: [
-        {
-          target: {
-            skill: ""
-          },
-          value: "",
-          property: "skill",
-          children: [],
-          constraints: {
-            length: "skill must be longer than or equal to 1 characters"
-          }
-        }
-      ]
+      skills: list_of_skills.filter(item => item !== skill)
     });
+
+    expect(mockGet).toHaveBeenCalled();
+    expect(mockRemove).toHaveBeenCalled();
+    expect(mockAdd).not.toHaveBeenCalled();
   });
 });
--- src/routes/skills.ts
+++ src/routes/skills.ts
@@ -3,6 +3,8 @@ import Router from "koa-router";
 import { AddSkillRequest } from "../request/AddSkillRequest";
 import { validate } from "class-validator";
 import * as storage from "../storage/redis";
+import { DeleteSkillRequest } from "../request/DeleteSkillRequest";
+
 // import { redisStorage } from "../../src/storage/redis";
 
 const router = new Router();
@@ -46,6 +48,39 @@ router.post("/skills", async (ctx: Context) => {
       skills:
         // ctx.request.body.skill,
         await store.get(list_name)
+    };
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.delete(`/skills`, async (ctx: Context) => {
+  try {
+    const validatorOptions = {};
+
+    const skill = new DeleteSkillRequest();
+    skill.skill = ctx.request.body.name || "";
+
+    const errors = await validate(skill, validatorOptions);
+
+    if (errors.length > 0) {
+      ctx.status = 400;
+      ctx.body = {
+        status: "error",
+        data: errors
+      };
+
+      return ctx;
+    }
+
+    const list = "skill_list";
+    const store = storage.redisStorage();
+
+    store.remove(list, skill.skill);
+
+    ctx.status = 200;
+    ctx.body = {
+      skills: await store.get(list)
     };
   } catch (err) {
     console.error(err);
